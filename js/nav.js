7413c8512c508c6956d58b70bea27a60c4882d07
elliottnes <60632551+elliottnes@users.noreply.github.com>
2020-02-07 10:50:04 -0800

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
